3d75eba0d30a39e601078db2dc9237a84a22d7dc
Kale Leach <kleach1212@gmail.com>
2022-01-30 18:27:33 -0800

    commit tree 0a8fd0267fbcba0d6505eac1cb4a2fa91e479f2d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData (name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  })
+  .then( function(response) {
+    return response.json();
+  })
+  .then(function(data){
+    const bodyData = document.querySelector('body')
+    return bodyData.innerHTML = data.id
+  })
+  .catch(function(error){
+    alert('Unauthorized Access')
+    return document.body.innerHTML = error.message;
+
+  
+  })
+  
+}
+
+submitData("Steve", "steve@steve.com" )
+submitData("Sam", "sam@sam.com" )
+
+
+
